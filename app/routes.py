04196ca5785389128cda594244f154e42aef5062
Joma Simeon <simjoms@gmail.com>
2018-10-01 12:32:45 +0300

    defined routes

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect
+
+# create flask app
+app = Flask(__name__)
+
+
+# define endpoints
+@app.route('/')
+def home():
+    return "welcome to stackoverflow-lite"
+
+
+@app.route('/api/v1/sign-up/', strict_slashes=False)
+def register():
+    return "welcome to sign up"
+
+
+@app.route('/api/v1/login/', strict_slashes=False)
+def login():
+    return "register here"
