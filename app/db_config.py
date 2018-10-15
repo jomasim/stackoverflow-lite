6a09278d63bca43b703d6a2cb247aa3b91ca4670
Joma Simeon <simjoms@gmail.com>
2018-10-15 20:26:56 +0300

    updated db config

--- app/db_config.py
+++ app/db_config.py
@@ -1,14 +1,14 @@
 from psycopg2 import connect
 from utils import env
 
-class DBconnection():
-    @staticmethod
-    def get_connection():
-        __connection = None
-        conn = connect(host=env('HOST'),
-                       database=env('DATABASE'),
-                       user=env('USER'),
-                       password=env('PASS'))
-        return conn
+# class DBconnection():
+#     # @staticmethod
+#     # def get_connection():
+#     #     __connection = None
+#     #     conn = connect(host=env('HOST'),
+#     #                    database=env('DATABASE'),
+#     #                    user=env('USER'),
+#     #                    password=env('PASS'))
+#     #     return conn
 
 
